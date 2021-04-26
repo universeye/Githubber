cb32c1b6a0b5c28c8414e2f89b5dad5cd3d45971
Terry <terrykuo501@gmail.com>
2021-04-26 15:19:03 +0800

    Add GFTextField

---
+++ Githubber/GFTextField.swift
@@ -0,0 +1,40 @@
+//
+//  GFTextField.swift
+//  Githubber
+//
+//  Created by Terry Kuo on 2021/4/26.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor //do bordoer color, need to use CGColor not UIColor, so you have to type out full UIColor
+        
+        textColor = .label //black on lightmode, white on darkmode
+        tintColor = .label //blinking cursur
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a usename"
+    }
+    
+}
