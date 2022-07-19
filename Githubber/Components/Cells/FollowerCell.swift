1dc4c9c1da383fb510ec185cda240d8e7262e286
“universeye” <terrykuo501@gmail.com>
2022-07-19 15:40:49 +0800

    fixed follwercell image broken.

--- Githubber/Components/Cells/FollowerCell.swift
+++ Githubber/Components/Cells/FollowerCell.swift
@@ -36,18 +36,18 @@ class FollowerCell: UICollectionViewCell {
         NSLayoutConstraint.activate([
             //avatarImageView
             //top
-            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: padding),
             //left and right, which give us our width base on how wide the cells are
-            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
-            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
             //we want the height to equal to thr height bc we want it to be a square
             avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
             
             
             //userNameLabel
             userNameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: padding + 4),
-            userNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
-            userNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            userNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            userNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
             userNameLabel.heightAnchor.constraint(equalToConstant: 20) //a little larger then the font size
         ])
     }
